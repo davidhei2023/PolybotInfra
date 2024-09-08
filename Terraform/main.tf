8b76162ccc1a11fab086b08b2fbdb7dbcafc992c
davidhei <davidhei2010@gmail.com>
2024-09-08 15:12:52 +0300

    kubeadm version 1

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,17 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# EKS Cluster
-resource "aws_eks_cluster" "k8s" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = var.public_subnet_ids
-    security_group_ids = var.control_plane_sg_ids
-  }
-}
-
+# IAM Role for Control Plane
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-eks-cluster-role"
 
@@ -30,26 +20,12 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
-resource "aws_eks_node_group" "k8s_nodes" {
-  cluster_name    = aws_eks_cluster.k8s.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.public_subnet_ids
-
-  scaling_config {
-    desired_size = var.desired_capacity
-    max_size     = var.max_capacity
-    min_size     = var.min_capacity
-  }
-
-  instance_types = [var.instance_type]
-
-  remote_access {
-    ec2_ssh_key = var.key_pair_name
-    source_security_group_ids = var.worker_node_sg_ids
-  }
+resource "aws_iam_instance_profile" "control_plane_iam_role" {
+  name = "${var.cluster_name}-control-plane-iam-role"
+  role = aws_iam_role.eks_cluster_role.name
 }
 
+# IAM Role for Worker Nodes
 resource "aws_iam_role" "eks_node_role" {
   name = "${var.cluster_name}-eks-node-role"
 
@@ -67,18 +43,7 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-# Attach necessary policies to the roles
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+resource "aws_iam_instance_profile" "worker_node_iam_role" {
+  name = "${var.cluster_name}-worker-node-iam-role"
+  role = aws_iam_role.eks_node_role.name
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,16 +1,19 @@
 variable "cluster_name" {
   description = "The name of your EKS cluster"
   type        = string
+  default     = "davidhei-eks-cluster"
 }
 
 variable "aws_region" {
   description = "The AWS region to deploy in"
   type        = string
+  default     = "us-east-2"
 }
 
 variable "public_subnet_ids" {
   description = "List of public subnet IDs"
   type        = list(string)
+  default     = ["subnet-003d39a7e2f59f7ea", "subnet-04000561778b910f1", "subnet-0b1b3ede9f956fd5d"]
 }
 
 variable "desired_capacity" {
@@ -40,14 +43,35 @@ variable "instance_type" {
 variable "key_pair_name" {
   description = "The name of the key pair to use for SSH access to the EC2 instances"
   type        = string
+  default     = "davidhei-ohio-key"
 }
 
 variable "control_plane_sg_ids" {
   description = "The IDs of the security groups to attach to the control-plane node"
   type        = list(string)
+  default     = ["sg-0e25deee5189c83d9"]
 }
 
 variable "worker_node_sg_ids" {
   description = "The IDs of the security groups to attach to the worker nodes"
   type        = list(string)
+  default     = ["sg-0e25deee5189c83d9"]
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the worker nodes"
+  type        = string
+  default     = "ami-0862be96e41dcbf74"  # Added the AMI ID from the merged config
+}
+
+variable "control_plane_iam_role" {
+  description = "The IAM role to use for the EKS control plane"
+  type        = string
+  default     = "davidhei-k8s-control-plane-role"  # Added control plane IAM role
+}
+
+variable "worker_node_iam_role" {
+  description = "The IAM role to use for the EKS worker nodes"
+  type        = string
+  default     = "davidhei-k8s-worker-node-role"  # Added worker node IAM role
 }
